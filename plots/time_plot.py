f6a72dbd8fbd086d31c7c1d7555953ffd7ba0553
Nicholas Orndorff <norndorff@live.com>
2023-08-19 14:41:25 -0700

    time plot

--- plots/time_plot.py
+++ plots/time_plot.py
@@ -1,5 +1,43 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
 
 
-motor = 0.001592399989021942
-prop = 0.0027942000015173107
-aero = 0.00012549999519251287
+
+motor = 0.320767147999959
+aero = 0.01248979999999733
+prop = 0.02558119999594055
+
+motor_s = 0.001592399989021942
+prop_s = 0.0027942000015173107
+aero_s = 0.00012549999519251287
+
+
+val = ['motor model', 'motor surrogate', 'rotor model', 'rotor surrogate', 'VLM model', 'VLM surrogate']
+time = [motor, motor_s, prop, prop_s, aero, aero_s]
+
+c1 = 'darkgray'
+c2 = 'royalblue'
+colors = [c1,c2,c1,c2,c1,c2]
+
+ec1 = 'dimgray'
+ec2 = 'darkblue'
+
+ec = [ec1,ec2,ec1,ec2,ec1,ec2]
+
+
+fig = plt.figure(figsize=(4,1.75))
+fontsize = 16
+
+plt.bar(val, time, color=colors, hatch='//', width=0.6, edgecolor=ec, log=True, zorder=2)
+
+plt.xticks(rotation=16)
+
+plt.grid(color='lavender', zorder=0, axis='y')
+
+plt.ylabel('Model Evaluation Time (s)')
+
+
+plt.savefig('time.pdf', transparent=True, bbox_inches="tight")
+plt.show()
